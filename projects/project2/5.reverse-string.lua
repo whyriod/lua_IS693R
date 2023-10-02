eb3f3303f7e725fc26d690ececc4ec92d46b659f
Caleb Siebach <whyriod@gmail.com>
2023-10-02 16:14:52 -0600

    adding some stuff

---
+++ projects/project2/5.reverse-string.lua
@@ -0,0 +1,14 @@
+--[[----------------------------------------------------------------------------
+    File: reverse-string.lua
+    Summary: Return string reverse function.
+]]------------------------------------------------------------------------------
+
+--[[----------------------------------------------------------------------------
+    Function: anonymous
+    Summary: reverses the string and returns it.
+    @param s {string} - The string to reverse
+    @return {string} - The reversed string
+]]------------------------------------------------------------------------------
+return function(s)
+    return string.reverse(s)
+end
---
+++ projects/project4/bank-account.lua
@@ -0,0 +1,37 @@
+--[[----------------------------------------------------------------------------
+    File: difference-of-squares.lua
+    Summary: Creates 3 functions, and returns them. One for sum of squares,
+        one for square of sum, and one for the difference.
+]]------------------------------------------------------------------------------
+
+local BankAccount = { balance = 0 }
+
+--[[----------------------------------------------------------------------------
+    Function: square_of_sum
+    Summary: Sums numbers 1...n and then squares the sum.
+    @param n {number} - 1...n numbers to sum.
+    @return {number} - The square_of_sum.
+]]------------------------------------------------------------------------------
+local function BankAccount.Balance(){
+    return BankAccount.balance
+}
+
+--[[----------------------------------------------------------------------------
+    Function: sum_of_squares
+    Summary: Squares numbers 1...n and then sums them.
+    @param n {nnumber} - 1...n numbers to square then sum.
+    @return {number} - The sum_of_squares.
+]]------------------------------------------------------------------------------
+
+
+--[[----------------------------------------------------------------------------
+    Function: difference_of_squares
+    Summary: Calculate the difference between the square_of_sum and the
+        sum_of_square
+    @param n {nnumber} - 1...n numbers to calculate the square_of_sum and the
+        sum_of_square for.
+    @return {number} - The difference between the square_of_sum and 
+        sum_of_square.
+]]------------------------------------------------------------------------------
+
+return BankAccount
